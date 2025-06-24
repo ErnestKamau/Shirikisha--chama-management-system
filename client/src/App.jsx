d711379cb02ab057878e703a003c0f882fc1419b
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-24 16:27:11 +0300

    add app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
