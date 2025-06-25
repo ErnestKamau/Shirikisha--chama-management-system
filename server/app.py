174398e895dfa014919889cf347d6b71b81028e4
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-25 08:05:09 +0300

    adds dashboard routes

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, make_response, request, jsonify
 from flask_restful import Api, Resource
 from config import app, api, db, jwt, bcrypt
 from controllers.auth_controllers import Register, UserLogin
+from controllers.dashboard import Dashboard
 from controllers.chama_group_controller import CreateChamaGroup, GetGroupMembers, AddUserToGroup, ChangeMemberRole, RemoveMember
 
 @app.errorhandler(404)
@@ -19,6 +20,7 @@ class Home(Resource):
 api.add_resource(Home, '/api/home', endpoint='home')
 api.add_resource(Register, '/api/register', endpoint='register')
 api.add_resource(UserLogin, '/api/login', endpoint='login')
+api.add_resource(Dashboard, '/api/dashboard', endpoint='dashboard')
 api.add_resource(CreateChamaGroup, '/api/group', endpoint='group')
 api.add_resource(GetGroupMembers, '/api/group/<int:group_id>/members', endpoint='group_members')
 api.add_resource(AddUserToGroup, '/api/group/<int:group_id>/members/<int:person_id>', endpoint='add_member')
