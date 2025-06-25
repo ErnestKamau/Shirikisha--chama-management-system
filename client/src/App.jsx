39e0e3df9d95c5326e503d064a6fcfdf58d79814
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-25 08:08:28 +0300

    refactoring code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
-import { useEffect } from "react"
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Features from './components/features/Features';
-import CTA from './components/CTA';
+import Cta from './components/CTA';
 import Footer from './components/Footer';
 
 function App() {
 
   useEffect(() => {
-    fetch("/api/users")
+    fetch("/api/home")
       .then((res) => res.json())
       .then((data) => {
         console.log(data)
@@ -22,7 +21,7 @@ function App() {
             <main>
                 <Hero />
                 <Features />
-                <CTA />
+                <Cta />
             </main>
             <Footer />
         </div>
