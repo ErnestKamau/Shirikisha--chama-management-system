d1477781eae6be33cd22494f88105f65d84d39b4
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-26 09:52:12 +0300

    merging

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -78,7 +78,6 @@ try {
           value={formData.email}
           autoComplete='email'
           onChange={handleInputChange}
-           autoComplete='email'
           className="w-full px-3 py-2 border border-gray-300 rounded-md"
         />
       </div>
