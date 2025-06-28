30b670e5dfd46363b5e8ef05da75c14e7651bddf
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-28 23:22:35 +0300

    editing vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  server:{
+    port:4000,
+    proxy:{
+      '/api':{
+        target: 'http://127.0.0.1:5555',
+        changeOrigin: true,
+        secure: false,
+      },
     },
+
   },
 })
