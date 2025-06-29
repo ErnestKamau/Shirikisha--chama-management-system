9832c64912236244f48f99da9ba4625f2eb4975b
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-29 14:47:08 +0300

    editing base url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:5555/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
